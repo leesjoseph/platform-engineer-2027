d9fb19d5962abf3b3fe34a3327c3671b49515719
Sea Lee <leesjoseph@pm.me>
2026-06-10 11:30:38 +0900

    day02: providers and resources.

---
+++ terraform/day02/main.tf
@@ -0,0 +1,41 @@
+terraform {
+    required_providers {
+        random = {
+            source = "hashicorp/random"
+        }
+    }
+}
+
+provider "random" {}
+
+resource "random_pet" "example" {
+    length = 2
+}
+
+resource "random_pet" "pet1" {
+  length = 2
+}
+
+resource "random_pet" "pet2" {
+  length = 2
+}
+
+resource "random_pet" "pet3" {
+  length = 2
+}
+
+output "pet_name" {
+    value = random_pet.example.id
+}
+
+output "pet_name1" {
+    value = random_pet.pet1.id
+}
+
+output "pet_name2" {
+    value = random_pet.pet2.id
+}
+
+output "pet_name3" {
+    value = random_pet.pet3.id
+}
